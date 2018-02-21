38be4a76fc9884c4a039d4b3002d8ed22f8d0cce
François Baldassari <francois.baldassari@oculus.com>
2018-02-20 20:59:25 -0800

    Implement item-add API

--- src/simple.c
+++ src/simple.c
@@ -7,17 +7,20 @@
 #include <string.h>
 
 // Hosts
-const char *HOST_AUTH = "https://auth.simperium.com/1/";
-const char *HOST_API = "https://api.simperium.com/1/";
+const char *HOST_AUTH = "https://auth.simperium.com/1";
+const char *HOST_API = "https://api.simperium.com/1";
 
 // Endpoints
-const char *ENDPOINT_AUTH = "/authorize/";
+const char *ENDPOINT_AUTH = "authorize";
+const char *ENDPOINT_ITEM = "i";
 
 // Limits
 #define MAX_APP_NAME_LEN 120
 #define MAX_API_KEY_LEN 64
 #define MAX_TOKEN_LEN 64
 #define MAX_URL_LEN 300
+#define MAX_BKT_NAME_LEN 120
+#define MAX_ITEM_ID_LEN 64
 
 // Data structures
 struct simperium_app {
@@ -26,8 +29,20 @@ struct simperium_app {
     CURL *curl;
 };
 
+// XXX does the session need to keep track of open buckets?
 struct simperium_session {
+    struct simperium_app *app;
     char token[MAX_TOKEN_LEN];
+};
+
+struct simperium_bucket {
+    struct simperium_session *session;
+    char name[MAX_BKT_NAME_LEN];
+};
+
+struct simperium_item {
+    char id[MAX_ITEM_ID_LEN];
+    const char *data;
 };
 
 struct response_data {
@@ -35,28 +50,32 @@ struct response_data {
     char *buffer;
 };
 
-// Helper functions
-void
-prv_build_url(char *url_out, const char *app_name, const char *host, const char *endpoint)
-{
-    strcat(url_out, host);
-    strcat(url_out, app_name);
-    strcat(url_out, endpoint);
-}
-
 static size_t
 prv_auth_callback(void *contents, size_t size, size_t nmemb, void *userp)
 {
     size_t size_bytes = size * nmemb;
-    struct response_data *rd = userp;
+    struct response_data *rd = (struct response_data *)userp;
 
     rd->buffer = realloc(rd->buffer, rd->bytes_written + size_bytes + 1);
     assert(rd->buffer != NULL);
 
     memcpy(&rd->buffer[rd->bytes_written], contents, size_bytes);
     rd->bytes_written += size_bytes;
+    rd->buffer[rd->bytes_written] = 0;
 
     return size_bytes;
+}
+
+static void
+prv_add_auth_header(struct simperium_session *session)
+{
+    char auth_hdr[MAX_TOKEN_LEN + 20];
+    sprintf(auth_hdr, "X-Simperium-Token: %s", session->token);
+
+    struct curl_slist *headers = NULL;
+    // XXX should probably add other headers (e.g. content-type)
+    headers = curl_slist_append(headers, auth_hdr);
+    curl_easy_setopt(session->app->curl, CURLOPT_HTTPHEADER, headers);
 }
 
 // Public API
@@ -104,17 +123,19 @@ simperium_app_deinit(struct simperium_app *app)
 struct simperium_session *
 simperium_session_open(struct simperium_app *app, const char *user, const char *passwd)
 {
-    // TODO validate user/passwd lengths
-    if (app == NULL) {
-        return NULL;
-    }
+    assert(app != NULL);
 
     struct simperium_session *session = calloc(sizeof(struct simperium_session), 1);
     assert(session != NULL);
 
+    session->app = app;
+
+    // libcurl persists options between calls
+    curl_easy_reset(app->curl);
+
     // Set URL
     char url[MAX_URL_LEN] = {0};
-    prv_build_url(url, app->name, HOST_AUTH, "/authorize/");
+    sprintf(url, "%s/%s/%s/", HOST_AUTH, app->name, ENDPOINT_AUTH);
     curl_easy_setopt(app->curl, CURLOPT_URL, url);
 
     // Set POST data
@@ -128,14 +149,15 @@ simperium_session_open(struct simperium_app *app, const char *user, const char *
     json_decref(req_json);
 
     // Set callback to handle response data
-    struct response_data resp_data = {0};
+    struct response_data resp_data;
+    resp_data.bytes_written = 0;
     resp_data.buffer = malloc(1);
     curl_easy_setopt(app->curl, CURLOPT_WRITEFUNCTION, prv_auth_callback);
     curl_easy_setopt(app->curl, CURLOPT_WRITEDATA, &resp_data);
 
     // Run request
     CURLcode res = curl_easy_perform(app->curl);
-    if(res != CURLE_OK) {
+    if (res != CURLE_OK) {
         fprintf(stderr, "curl_easy_perform() failed: %s\n",
                 curl_easy_strerror(res));
         goto error;
@@ -160,7 +182,7 @@ simperium_session_open(struct simperium_app *app, const char *user, const char *
     strcpy(session->token, tk);
     json_decref(resp_json);
 
-    printf("Found token %s\n", session->token);
+    printf("User token is %s\n", session->token);
 
 error:
     free(req_data);
@@ -172,6 +194,64 @@ void
 simperium_session_close(struct simperium_session *session)
 {
     free(session);
+}
+
+struct simperium_bucket *
+simperium_bucket_open(struct simperium_session *session, const char *bucket_name)
+{
+    assert(session != NULL);
+
+    struct simperium_bucket *bucket = calloc(sizeof(struct simperium_bucket), 1);
+    assert(bucket != NULL);
+
+    // XXX client_id ?
+    bucket->session = session;
+    strncpy(bucket->name, bucket_name, MAX_BKT_NAME_LEN);
+
+    return bucket;
+}
+
+void
+simperium_bucket_close(struct simperium_bucket *bucket)
+{
+    free(bucket);
+}
+
+int
+simperium_bucket_add_item(struct simperium_bucket *bucket, struct simperium_item *item)
+{
+    struct simperium_app *app = bucket->session->app;
+    char url[MAX_URL_LEN] = {0};
+    sprintf(url, "%s/%s/%s/%s/%s", HOST_API,
+                                   app->name,
+                                   bucket->name,
+                                   ENDPOINT_ITEM,
+                                   item->id);
+
+    // libcurl persists options between calls
+    curl_easy_reset(app->curl);
+    prv_add_auth_header(bucket->session);
+    curl_easy_setopt(app->curl, CURLOPT_URL, url);
+    curl_easy_setopt(app->curl, CURLOPT_POSTFIELDS, item->data);
+
+    CURLcode res = curl_easy_perform(app->curl);
+    if (res != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+    }
+    printf("\n");
+}
+
+int
+simperium_bucket_get_item(struct simperium_bucket *bucket, struct simperium_item *item)
+{
+
+}
+
+int
+simperium_bucket_remove_item(struct simperium_bucket *bucket, struct simperium_item *item)
+{
+
 }
 
 int
@@ -230,8 +310,21 @@ main(int argc, char **argv)
         goto deinit_app;
     }
 
-    simperium_session_close(session);
+    struct simperium_bucket *todo_bkt = simperium_bucket_open(session, "todo");
+    if (!todo_bkt) {
+        printf("Failed to open todo bucket\n");
+        exitcode = 1;
+        goto close_session;
+    }
 
+    struct simperium_item item = {
+        .id = "12345678",
+        .data = "{\"title\": \"Watch Battle Royale\",\"done\": false}",
+    };
+    simperium_bucket_add_item(todo_bkt, &item);
+
+close_session:
+    simperium_session_close(session);
 deinit_app:
     simperium_app_deinit(app);
 exit:
